8795ed34045b27229b744cde2731f85e3e349d52
Curso <Curso@PORT-Z14.ipartekaula.com>
2018-07-11 14:05:58 +0200

    commitInicial

---
+++ Ejemplo1/src/com/ipartek/formacion/ejemplo1/Prueba1.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.ejemplo1;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[2];
+		arr[2] = 5;
+		punto p = new Punto(x, y)
+	}
+
+}
